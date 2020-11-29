9b216f2382c0f9b645e6baf523e4d3b365f3e975
dulcerussel <54066608+dulcerussel@users.noreply.github.com>
2020-11-29 11:49:05 +0800

    Add files via upload

---
+++ include/footer.php
@@ -0,0 +1,3 @@
+	<div class="footer-wrap bg-white pd-20 mb-20 border-radius-5 box-shadow">
+		BRGY4ALL - A Platform for Public-Private Social Service Distribution</a>
+	</div>
---
+++ include/sidebar.php
@@ -0,0 +1,75 @@
+	<div class="left-side-bar">
+		<div class="brand-logo">
+			<a href="index.php">
+				<img src="vendors/images/deskapp-logo.png" alt="">
+			</a>
+		</div>
+		<div class="menu-block customscroll">
+			<div class="sidebar-menu">
+				<ul id="accordion-menu">
+					<li>
+						<a href="javascript:;" class="dropdown-toggle no-arrow">
+							<span class="fa fa-home"></span><span class="mtext">Home</span>
+						</a>
+					</li>
+					<li>
+						<a href="form-wizard.php" class="dropdown-toggle no-arrow">
+							<span class="fa fa-pencil"></span><span class="mtext">Registration Form</span>
+						</a>
+					</li>
+					<li>
+						<a href="javascript:;" class="dropdown-toggle no-arrow">
+							<span class="fa fa-table"></span><span class="mtext">Masterlist</span>
+						</a>
+					</li>
+					<li>
+						<a href="calendar.php" class="dropdown-toggle no-arrow">
+							<span class="fa fa-calendar-o"></span><span class="mtext">Calendar</span>
+						</a>
+					</li>
+					<li>
+						<a href="javascript:;" class="dropdown-toggle no-arrow">
+							<span class="fa fa-desktop"></span><span class="mtext"> Make BRGY Announcement</span>
+						</a>
+					</li>
+					<li>
+						<a href="javascript:;" class="dropdown-toggle no-arrow">
+							<span class="fa fa-desktop"></span><span class="mtext"> Make BRGY Service </span>
+						</a>
+					</li>
+					<li class="dropdown">
+						<a href="javascript:;" class="dropdown-toggle">
+							<span class="fa fa-list"></span><span class="mtext">NGOs and National Collab</span>
+						</a>
+						<ul class="submenu">
+							<li><a href="javascript:;">Level 1</a></li>
+							<li><a href="javascript:;">Level 1</a></li>
+							<li><a href="javascript:;">Level 1</a></li>
+							<li class="dropdown">
+								<a href="javascript:;" class="dropdown-toggle">
+									<span class="fa fa-plug"></span><span class="mtext">Level 2</span>
+								</a>
+								<ul class="submenu child">
+									<li><a href="javascript:;">Level 2</a></li>
+									<li><a href="javascript:;">Level 2</a></li>
+								</ul>
+							</li>
+							<li><a href="javascript:;">Level 1</a></li>
+							<li><a href="javascript:;">Level 1</a></li>
+							<li><a href="javascript:;">Level 1</a></li>
+						</ul>
+					</li>
+					<li>
+						<a href="chat.php" class="dropdown-toggle no-arrow">
+							<span class="fa fa-comments-o"></span><span class="mtext">Chat</span></span>
+						</a>
+					</li>
+					<li>
+						<a href="invoice.php" class="dropdown-toggle no-arrow">
+							<span class="fa fa-map-o"></span><span class="mtext">Invoice</span>
+						</a>
+					</li>
+				</ul>
+			</div>
+		</div>
+	</div>
